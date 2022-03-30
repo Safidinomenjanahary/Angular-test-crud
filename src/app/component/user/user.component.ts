196892fe8d371ff0c1ab72fc9a5f12bd50a7f819
Safidinomenjanahary <safidyherimampianina@gmail.com>
2022-03-30 09:54:26 +0300

    final version

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -14,8 +14,10 @@ export class UserComponent implements OnInit {
   prenom_input_text:any;
   date_naissance_input_text:any;
   classe_input_text:any;
-  data_user:any;
+  data_user:any=[];
   user_for_deleting:any;
+  data_is_vide:boolean=false;
+  user_to_update:any=[];
   constructor(private userService:UserService) {}
 
   ngOnInit(): void {
@@ -23,32 +25,65 @@ export class UserComponent implements OnInit {
   }
 
   insertUser(){
-    let id=this.id_input_text;
-    let nom=this.nom_input_text;
-    let prenom=this.prenom_input_text;
-    let date_naissance=this.date_naissance_input_text;
-    let classe=this.classe_input_text;
-    let user=new User(id,nom,prenom,date_naissance,classe);
+    this.id_input_text=this.data_user.length+1;
+    var user=new User(this.id_input_text,this.nom_input_text,this.prenom_input_text,this.date_naissance_input_text,this.classe_input_text);
     this.userService.insert_user(user);
     this.ngOnInit();
   }
   findAllUser(){
-
-    var data:any=this.userService.findAllUser();
+    var data=this.userService.findAllUser();
     console.log(data);
-    let result=[];
-    for(let a=1;a<data.length;a++){
-      result[a]=JSON.parse(data[a]);
-      
+    var result=[];
+    if(data!=="vide"){
+      for(var a=0;a<data.length;a++){
+        console.log("i="+a);
+        if(data[a]=="vide"){
+          console.log("VIDEEEE");
+          a++;
+        }
+        else{
+          var userObject=JSON.parse(data[a]);
+          result[a]=this.json2array(userObject);
+        }
+      }
+      this.data_user=result;
+      this.data_is_vide=false;
     }
-    console.log(result[1].nom);
-    this.data_user=result;
+    else if(data=="vide"){
+      this.data_is_vide=true;
+    }
   }
   deleteUser(userId:any){
     this.userService.deleteUser(userId);
     this.ngOnInit();
   }
-  addObjectInLocalStorage(object:string){
-    localStorage.getItem('nombre')
+  deleteLocalStorage(){
+    this.userService.deleteStorage();
+    this.ngOnInit();
+  }
+  json2array(json:any){
+    var result :any= [];
+    var keys = Object.keys(json);
+    keys.forEach(function(key){
+        result.push(json[key]);
+    });
+    return result;
+  }
+  findUserById(userId:any){
+    var user:any=this.userService.findUserById(userId);
+    return user;
+  }
+  updateUserToUpdate(userId:any){
+    var user_ancien:any=this.findUserById(userId);
+    var userObject=JSON.parse(user_ancien);
+    this.user_to_update=this.json2array(userObject);
+  }
+  updateUser(userId:any){
+    this.updateUserToUpdate(userId);
+  }
+  finalUpdatingUser(){
+    var newUser=new User(this.user_to_update[0],this.user_to_update[1],this.user_to_update[2],this.user_to_update[3],this.user_to_update[4]);
+    this.userService.updateUser(newUser);
+    this.ngOnInit();
   }
 }
--- src/app/sevice/user.service.ts
+++ src/app/sevice/user.service.ts
@@ -6,41 +6,49 @@ import { User } from '../component/class/user';
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { 
-    this.findAllUser();
-  }
-
+  constructor() {}
   insert_user(user:User){
-      let user_number=JSON.parse(JSON.stringify(localStorage.getItem("nombre")));
-      if(localStorage.getItem("nombre")==="" || localStorage.getItem("nombre")===null || localStorage.getItem("nombre")==='NaN' || localStorage.getItem("nombre")==="NaN"){
-        user_number="0";
+      var user_number=JSON.parse(JSON.stringify(localStorage.getItem("nombre")));
+      var number=1;
+      if(user_number>0 && user_number!=null){
+        number=Number(user_number)+1;
       }
-      let number=Number(user_number)+1;
       localStorage.setItem("user"+(number),JSON.stringify(user));
       localStorage.setItem("nombre",""+number);
-      
   }
   findAllUser(){
-    let user_number=Number(localStorage.getItem('nombre'));
-    if(user_number==null || user_number==0){
-      user_number=0;
+    var user_number=Number(localStorage.getItem("nombre"));
+    var data:any=[];
+    var all_data_is_null=true;
+    for(var i=0;i<user_number;i++){
+      var indice=i+1;
+      if(localStorage.getItem("user"+indice)!=null)
+      {
+        data[i]=localStorage.getItem("user"+indice);
+        all_data_is_null=false;
+      }
+      else if(localStorage.getItem("user"+indice)==null){
+        data[i]="vide";
+      }
     }
-    localStorage.clear();
-    let data=[];
-    for(let i=1;i<user_number;i++){
-      data[i]=localStorage.getItem("user"+i);
+    if(all_data_is_null){
+      console.log("vide");
+      return "vide";
     }
     return data;
   }
+
   deleteUser(todoText:any) {
-    localStorage.removeItem(""+todoText);
+    console.log("user to delete= user"+todoText);
+    localStorage.removeItem("user"+todoText);
   }
-  updateUser(oldText:any, newText:any){
-    // let todos = JSON.parse(localStorage.getItem('todos'));
-    // for(let i = 0; i <todos.length; i++) {
-    //  if(todos[i].text == oldText) {
-    //    todos[i].text = newText;
-    //  }
+  deleteStorage(){
+    localStorage.clear();
+  }
+  findUserById(userId:any){
+    return localStorage.getItem("user"+userId);
+  }
+  updateUser(user:User){
+    localStorage.setItem("user"+user.id,JSON.stringify(user));
   }
 }
